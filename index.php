4bc5592ed9dddab9d7f2242edd8c07d89be67e67
Sharmaji007a <13bhanusharma@gmail.com>
2024-12-19 19:59:14 +0530

    php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<title>Bhanu Uday Sharma PHP</title>
+<h1>Bhanu Uday Sharma PHP</h1>
+<p>
+    The SHA256 hash of Bhanu Uday Sharma is 
+    <?php 
+        echo hash('sha256', 'Bhanu Uday Sharma'); 
+    ?>
+</p>
+<pre>ASCII ART:
+    ******  
+    **    **
+    **    **
+    ******  
+    **    **
+    **    **
+    ******  
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
